b78a2153ab4de10f17629238766d58bc5051f5ff
Justin Palmer <justin@labratrevenge.com>
2015-07-08 12:50:47 -0700

    add some copy and paste queries

---
+++ oregon-fire/sql/queries.sql
@@ -0,0 +1,11 @@
+--- Find what years the dataset covers
+select distinct(year) from fires order by 1 desc;
+
+--- Find what causes fires.  Note the empty fields.
+select distinct(cause) from fires;
+
+--- Count and rank the causes
+select count(*), cause from fires group by 2 order by 1 desc;
+
+-- Count and rank the causes in 2014
+select count(*), cause from fires where year = 2014 group by 2 order by 1 desc;
